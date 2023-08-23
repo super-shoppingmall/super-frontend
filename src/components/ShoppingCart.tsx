be39a77b79041034b57634b5c58a366754b4e0e8
Ye Seul Kim <yskim940914@gmail.com>
2023-08-23 23:39:53 +0900

    Chore: eslint & prettier

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 const ShoppingCart = () => {
-  return <h1>물품 주문 페이지입니다.</h1>;
+	return <h1>물품 주문 페이지입니다.</h1>;
 };
 
 export default ShoppingCart;
