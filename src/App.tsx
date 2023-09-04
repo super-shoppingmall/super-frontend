23fcdb4797b6bbdb29d66f58e6865370c5b9d441
Ye Seul Kim <yskim940914@gmail.com>
2023-09-04 23:04:36 +0900

    Feat: 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,22 @@ import ProductDetail from './components/ProductDetail';
 import ProductRegister from './components/ProductRegister';
 import Profile from './components/Profile';
 import ShoppingCart from './components/ShoppingCart/ShoppingCart';
-import Signup from './components/Signup';
 
 import Login from './pages/Login';
+import Signup from './pages/Signup';
 import CartContextProvider from './store/cart-context';
+import MobileAuthForm from './layouts/Signup/MobileAuthForm';
+import SignupForm from './layouts/Signup/SignupForm';
 
 const App: React.FC = () => {
 	return (
 		<Routes>
 			<Route path='/' element={<Main />} />
 			<Route path='/login' element={<Login />} />
-			<Route path='/signup' element={<Signup />} />
+			<Route path='/signup' element={<Signup />}>
+				<Route index element={<MobileAuthForm />} />
+				<Route path='step1' element={<SignupForm />} />
+			</Route>
 			<Route path='/detail' element={<ProductDetail />} />
 			<Route path='/register' element={<ProductRegister />} />
 			<Route path='/profile' element={<Profile />} />
