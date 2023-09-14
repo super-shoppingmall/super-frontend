dfe1dc4bb9e11e0f6db68b27f8cfbecb6488352b
sleedev <yeaji105@naver.com>
2023-09-14 23:26:39 +0900

    개별 컴포넌트에서 헤더 제거

--- src/components/OrderCompleted.tsx
+++ src/components/OrderCompleted.tsx
@@ -1,11 +1,6 @@
-import Header from './Header/Header';
-
 function OrderCompleted() {
 	return (
 		<div className='flex flex-col justify-center items-center gap-48'>
-			<div className='w-screen'>
-				<Header />
-			</div>
 			<div className='border border-gray-400 px-24 py-12 text-center' style={{ width: '70vw' }}>
 				주문이 성공적으로 처리되었습니다.
 			</div>
